b11dbde1d92011d0fa375b50b6967408480dfc10
nirmalwj <nirmalwishwajith2@gmail.com>
2025-11-05 01:16:33 -0800

    Update config.js

--- config.js
+++ config.js
@@ -6,9 +6,9 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "",
-ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/DANUWA-MD/DANUWA-MD/blob/main/images/DANUWA-MD.png?raw=true",
-ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 DANUWA-MD Is Alive Now😍*",
-BOT_OWNER: '94776121326',  // Replace with the owner's phone number
+ALIVE_IMG: process.env.ALIVE_IMG || "https://raw.githubusercontent.com/nirmalwj/-Nirmal-wishwajith-/refs/heads/main/images/OIP.webp",
+ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 Wishwa Bots Is Alive Now😍*",
+BOT_OWNER: '94769787408',  // Replace with the owner's phone number
 
 
 
